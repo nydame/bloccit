4de5150eb9874276b7e98e270093c0f26913345b
felicia <felicia@hyperarts.com>
2018-06-23 11:55:27 -0700

    Assignment 6 - Seeding Data: completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,35 @@ posts = Post.all
     )
 end
 
+# Create unique Post
+unique_post = Post.find_or_create_by!(
+    title: "I Am Truly Unique!",
+    body: "Aenean nec lacus vestibulum, blandit elit ut, dictum augue. Etiam consequat varius placerat."
+)
+
+def number_of_unique_posts(standard)
+    unique = Post.pluck(:title).select do |title|
+        title == standard
+    end
+    unique.count
+
+end
+
+# Create unique Comment associated with the unique Post
+Comment.find_or_create_by!(
+    post: unique_post,
+    body: "I would like to make a unique comment."
+)
+
+def number_of_unique_comments(standard)
+    unique = Comment.pluck(:body).select do |body|
+        body == standard
+    end
+    unique.count
+end
+
 puts "Seeding finished"
 puts "#{Post.count} posts created"
+puts "The number of posts entitled 'I Am Truly Unique!' is #{number_of_unique_posts('I Am Truly Unique!')}."
 puts "#{Comment.count} comments created"
+puts "The number of comments that say 'I would like to make a unique comment.' is #{number_of_unique_comments('I would like to make a unique comment.')}."
