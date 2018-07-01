cfc40fe6c63dd4561d2e7322f2e5597c67fff7f3
felicia <felicia@hyperarts.com>
2018-06-30 17:21:37 -0700

    Checkpoint 8 - New Advertisements: completed.

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -22,5 +22,9 @@ module RandomData
         letters.shuffle!
         letters[0, rand(3..8)].join
     end
+    # Return a random integer from 1 to 999
+    def self.random_price
+        rand(1...1000)
+    end
 
 end
