2f66aec9249f67adae7e19bce72bad2f98d5f568
felicia <felicia@hyperarts.com>
2018-07-06 17:28:08 -0700

    Halfway through code refactoring.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :topics
+  # resources :posts
+  resources :topics do
+      resources :posts, except: [:index]
+  end
+  
   get 'about' => 'welcome#about'
   root 'welcome#index' #same as root({to: 'welcome#index'})
  end
