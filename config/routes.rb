60bb7d553a6020edc4cca1d7e99afcb812daace9
felicia <felicia@hyperarts.com>
2018-07-11 10:44:38 -0700

    Assignment 13 - Authentication - Sign Up: completed.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
+  post 'users/confirm' => 'users#confirm'
   root 'welcome#index' #same as root({to: 'welcome#index'})
  end
