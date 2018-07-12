9464352e17d0839970fb7d56f330ebcc93dae21b
felicia <felicia@hyperarts.com>
2018-07-11 18:28:37 -0700

    Assignment 14 - Authentication - Signing In: completed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,16 @@ module ApplicationHelper
     def form_group_tag(errors, &block)
         css_class = 'form_group'
         css_class << ' has-error' if errors.any?
-        
+
         content_tag :div, capture(&block), class: css_class
+    end
+
+    def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{thumbnail_dimension}"
+    end
+
+    def thumbnail_dimension
+        48
     end
 end
