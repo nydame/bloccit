104a375cafc84085670b1fef787b0f633870cd51
felicia <felicia@hyperarts.com>
2018-07-21 16:02:42 -0700

    Checkpoint 19 - Favorites: create model and associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
+    has_many :favorites, dependent: :destroy
 
     validates :name, length: {minimum: 1, maximum: 100}, presence: true
     validates :password, presence: true, length: {minimum: 6}, if: -> {password_digest.nil?}
@@ -14,4 +15,9 @@ class User < ApplicationRecord
     has_secure_password
 
     enum role: [:admin, :member]
+
+    def favorite_for(post)
+      favorites.where(post_id: post.id).first
+    end
+
 end
