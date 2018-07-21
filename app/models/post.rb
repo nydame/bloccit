0d68d0ad22603e595b6c5db84b5a65f314653262
felicia <felicia@hyperarts.com>
2018-07-21 11:29:55 -0700

    Checkpoint 18 - Voting: completed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,4 +22,10 @@ class Post < ApplicationRecord
     def points
         votes.sum(:value)
     end
+
+    def update_rank
+      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+      new_rank = points + age_in_days
+      update_attribute(:rank, new_rank)
+    end
 end
